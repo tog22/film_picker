391ed372c70e3c9de2bbdcbbd23d202fde913f46
tog22 <tom.ash.github@philosofiles.com>
2022-01-18 18:18:57 -0800

    Specific validation for my form

--- Components/Account/Create/Form.js
+++ Components/Account/Create/Form.js
@@ -6,19 +6,53 @@ const Schema = Yup.object().shape({
   email: Yup.string().required('This field is required'),
 });
 
-const validate = values => {
-	return {
-	  zip: 'This field is required',
-	};
+
+// A custom validation function. This must return an object which keys are symmetrical to our values/initialValues
+
+const validate_signup = values => {
+	
+	const errors = {
+		full_name: null,
+		first_name: null,
+		password: null
+	}
+	
+	if (!values.full_name) {
+		errors.full_name = 'Required'
+	} else if (values.full_name.length > 40) {
+		errors.full_name = 'Must be 40 characters or less'
+	}
+
+	if (!values.first_name) {
+		errors.first_name = 'Required'
+	} else if (values.first_name.length > 20) {	
+		errors.first_name = 'Must be 20 characters or less'
+	}
+
+	if (!values.password) {
+		errors.password = 'Required';
+	} else if (values.first_name.length > 20) {	
+		errors.password = 'Must be 20 characters or less';
+	}
+
+	// if (!values.email) {
+	 // 	errors.email = 'Required';
+	// } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+	 // 	errors.email = 'Invalid email address';
+	// }
+
+	return errors
+
 }
 
+// Exra validation applied on a field-by-field basis for only required fields
 const isRequired = message => value => (!!value ? undefined : message);
 
 export const Signup = () => (
   <div>
 	<Formik
 	  validationSchema={Schema}
-	  validate={validate}
+	  validate={validate_signup}
 	  initialValues={{
 		full_name: '',
 		first_name: '',
