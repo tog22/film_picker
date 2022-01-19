1a07b19517123efb157f72b14f8c5e1f9f066214
tog22 <tom.ash.github@philosofiles.com>
2022-01-18 17:52:16 -0800

    Validation via Formik GitHub eg

--- Components/Account/Create/Form.js
+++ Components/Account/Create/Form.js
@@ -1,63 +1,43 @@
-import React from 'react'
-import { Formik, useFormik, Form, Field, ErrorMessage } from 'formik';
-//  npm install formik --save
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
+const Schema = Yup.object().shape({
+  email: Yup.string().required('This field is required'),
+});
 
-
-
-// A custom validation function. This must return an object
-// which keys are symmetrical to our values/initialValues
-const validate_signup = values => {
-	
-	const errors = {}
-	alert(1)
-	if (!values.full_name) {
-		errors.full_name = 'Required'
-	} else if (values.full_name.length > 40) {
-		errors.full_name = 'Must be 40 characters or less'
-	}
-	
-	if (!values.first_name) {
-		errors.first_name = 'Required'
-	} else if (values.first_name.length > 20) {	
-		errors.first_name = 'Must be 20 characters or less'
-	}
-	
-	if (!values.password) {
-		errors.password = 'Required';
-	} else if (values.first_name.length > 20) {	
-		errors.password = 'Must be 20 characters or less';
-	}
-	
-	// if (!values.email) {
-	 // 	errors.email = 'Required';
-	// } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-	 // 	errors.email = 'Invalid email address';
-	// }
-	
-	return errors
-	
+const validate = values => {
+	return {
+	  zip: 'This field is required',
+	};
 }
 
-export const Signup = () => {
-	
-	const formik = useFormik({
-		 initialValues: {
-		   full_name: '',
-		   first_name: '',
-		   password: ''
-		 },
-		 validate_signup,
-		 onSubmit: values => {
-		   alert(JSON.stringify(values, null, 2));
-		 },
-	   });
-	
-	return (
-		<form
+const isRequired = message => value => (!!value ? undefined : message);
+
+export const Signup = () => (
+  <div>
+	<Formik
+	  validationSchema={Schema}
+	  validate={validate}
+	  initialValues={{
+		full_name: '',
+		first_name: '',
+		password: ''
+	  }}
+	  onSubmit={values => {
+		alert(JSON.stringify(values, null, 2));
+	  }}
+	  render={({
+		errors,
+		touched,
+		setFieldValue,
+		setFieldTouched,
+		validateField,
+		validateForm,
+	  }) => (
+		<Form
 			id="create_account"
 			className="form1"
-			onSubmit={formik.handleSubmit}
 		>
 			<div className="s_input_and_label">
 				<label htmlFor="full_name">
@@ -68,21 +48,12 @@ export const Signup = () => {
 						Or other username - we recommend your full name so friends can recognize you quickly from it, even ones from new movie clubs you join later.
 					</div>
 				</label>
-				<input 
-					name="full_name" 
-					id="full_name" 
-					type="text" 
-					onChange={formik.handleChange}
-					value={formik.values.full_name}
+				<Field
+					name="full_name"
+					validate={isRequired('This field is required')}
+					type="text"
 				/>
-				{formik.errors.full_name 
-					?
-					<div className="error">
-						{formik.errors.full_name}
-					</div>
-					:
-					null
-				}
+				<ErrorMessage name="full_name" />
 			</div>
 			<div className="s_input_and_label">
 				<label htmlFor="first_name">
@@ -93,21 +64,12 @@ export const Signup = () => {
 						This'll show in a list of short names. If you prefer you can use another name, but it should be as short as a typical first name, and friends should quickly recognize you from from it.
 					</div>
 				</label>
-				<input 
-					name="first_name" 
-					id="first_name" 
-					type="text" 
-					onChange={formik.handleChange}
-					value={formik.values.first_name}
+				<Field
+					name="first_name"
+					validate={isRequired('This field is required')}
+					type="text"
 				/>
-				{formik.errors.first_name 
-					?
-					<div className="error">
-						{formik.errors.first_name}
-					</div>
-					:
-					null
-				}
+				<ErrorMessage name="first_name" />
 			</div>
 			<div className="s_input_and_label">
 				<label htmlFor="password">
@@ -115,28 +77,23 @@ export const Signup = () => {
 						Password
 					</div>
 				</label>
-				<input 
-					name="password" 
-					type="password" 
-					onChange={formik.handleChange}
-					value={formik.values.password}
+				<Field
+					name="password"
+					validate={isRequired('This field is required')}
+					type="text"
 				/>
-				{formik.errors.password 
-					?
-					<div className="error">
-						{formik.errors.password}
-					</div>
-					:
-					null
-				}
+				<ErrorMessage name="password" />
 			</div>
 			<div className="s_submit">
-				<input type="submit" value="Sign up" />
+				<button type="submit">
+					Submit
+				</button>
 			</div>
-		</form>
-	);
-	
-}
+		</Form>
+	  )}
+	/>
+  </div>
+);
 
 
 let l = function (to_log) { 
