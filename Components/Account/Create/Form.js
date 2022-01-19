ebaa69aa6feb65fb8e9a3d0ec62d030d20638eb7
tog22 <tom.ash.github@philosofiles.com>
2022-01-18 16:43:53 -0800

    → Formik (basic conversion)

--- Components/Account/Create/Form.js
+++ Components/Account/Create/Form.js
@@ -4,10 +4,25 @@ import { Formik, useFormik, Form, Field, ErrorMessage } from 'formik';
 
 export const Signup = () => {
 	
+	const formik = useFormik({
+		 initialValues: {
+		   full_name: '',
+		   first_name: '',
+		   password: ''
+		 },
+		 onSubmit: values => {
+		   alert(JSON.stringify(values, null, 2));
+		 },
+	   });
+	
 	return (
-		<form id="create_account" class="form1">
+		<form
+			id="create_account"
+			class="form1"
+			onSubmit={formik.handleSubmit}
+		>
 			<div class="s_input_and_label">
-				<label for="full_name">
+				<label htmlfor="full_name">
 					<div class="s_main">
 						Full name
 					</div>
@@ -15,10 +30,16 @@ export const Signup = () => {
 						Or other username - we recommend your full name so friends can recognize you quickly from it, even ones from new movie clubs you join later.
 					</div>
 				</label>
-				<input name="full_name" id="full_name" type="text" />
+				<input 
+					name="full_name" 
+					id="full_name" 
+					type="text" 
+					onChange={formik.handleChange}
+					value={formik.values.full_name}
+				/>
 			</div>
 			<div class="s_input_and_label">
-				<label for="first_name">
+				<label htmlfor="first_name">
 					<div class="s_main">
 						First name
 					</div>
@@ -26,15 +47,26 @@ export const Signup = () => {
 						This'll show in a list of short names. If you prefer you can use another name, but it should be as short as a typical first name, and friends should quickly recognize you from from it.
 					</div>
 				</label>
-				<input name="first_name" id="first_name" type="text" />
+				<input 
+					name="first_name" 
+					id="first_name" 
+					type="text" 
+					onChange={formik.handleChange}
+					value={formik.values.first_name}
+				/>
 			</div>
 			<div class="s_input_and_label">
-				<label for="password">
+				<label htmlfor="password">
 					<div class="s_main">
 						Password
 					</div>
 				</label>
-				<input name="password" type="password" />
+				<input 
+					name="password" 
+					type="password" 
+					onChange={formik.handleChange}
+					value={formik.values.password}
+				/>
 			</div>
 			<div class="s_submit">
 				<input type="submit" value="Sign up" />
