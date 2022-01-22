e5356596d58066e95236dc1e6e3863db70a66261
tog22 <tom.ash.github@philosofiles.com>
2022-01-21 17:52:24 -0800

    ***EG: Router now nested, with Chrome shared

--- App_Router.js
+++ App_Router.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes, Link, BrowserRouter } from 'react-router-dom'
 
+import Chrome from './Components/Chrome/Chrome'
 import GroupFilms from './Pages/Group_Films/Page'
 import CreateAccount from './Pages/Account/Create/Page'
 
@@ -9,18 +10,11 @@ export default class Router extends React.Component {
 	render() {
 		return (
 			<BrowserRouter>
-				<h1 id="site_title">
-					<Link to='/'>
-						🎞️ Film Picker
-					</Link>
-				</h1>
-				<Link to='/signup'>
-					Switch to Signup page
-				</Link>
 				<Routes>
-					<Route path='/' element={<GroupFilms />}></Route>
-					<Route path='/group' element={<GroupFilms />}></Route>
-					<Route path='/signup' element={<CreateAccount />}></Route>
+					<Route path='/' element={<Chrome />}>
+						<Route path='/group' element={<GroupFilms />}></Route>
+						<Route path='/signup' element={<CreateAccount />}></Route>
+					</Route>
 				</Routes>
 			</BrowserRouter>
 		)
