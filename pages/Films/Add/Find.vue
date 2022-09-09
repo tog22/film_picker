d4ed8b8f6a21227af6daa75e762d9c4291cad52d
tog22 <tom.ash.github@philosofiles.com>
2022-09-09 10:39:22 -0700

    + 'Find Film' page

---
+++ pages/Films/Add/Find.vue
@@ -0,0 +1,93 @@
+<template>
+<q-page>
+    <h1>
+        Find Film
+    </h1>
+    <q-form
+        @submit="on_submit"
+        class="full_width_form q-gutter-y-md"
+    >
+        <q-input
+            label="Enter search here"
+            v-model="search_term"
+            outlined
+        />
+        <div>
+            <q-radio
+                label="Title"
+                v-model="search_type"
+                val="title"
+            />
+            <q-radio
+                label="IMDB ID"
+                v-model="search_type"
+                val="imdb_id"
+            />
+        </div>
+        <div className="button_row tight_top">
+          <q-btn
+              type="submit"
+              label="Search"
+              color="primary"
+              size="md"
+          />
+        </div>
+        <q-banner id="banner" class="hidden text-white bg-red">
+            Please enter a title or IMDB ID
+        </q-banner>
+    </q-form>
+</q-page>
+</template>
+
+<script>
+import $ from 'jquery'
+import { inject } from "vue"
+export default {
+name:       'Add_Film_Page',
+components: {
+},
+methods: {
+    on_submit() {
+        let search_type = ((this.search_type === 'title') ? 's=' : 'i=')
+        let query_url = 'http://www.omdbapi.com/?apikey=67a0cf67&'+search_type+this.search_term
+        lo(query_url)
+        let fn = {
+            on_success(data, status, request) {
+                let response = JSON.parse(data)
+                if (response.Response === 'False') {
+                    switch (response.Error) {
+                        case 'Too many results.':
+                            alert('Too many results. Try searching for a more specific title, or search on IMDB [LINK] and get the IMDB ID (e.g. tt0800369) from the URL')
+                            break
+                        default:
+                            alert('Error: '+response.Error)
+                            break
+                    }
+                }
+                store.sections.add_film.search_results = response.Search
+                this.$router.push('/search_results')
+            },
+            users_error(request, status, error) {
+                alert('load users error')
+            },
+        }
+        $.ajax({
+            url:        query_url,
+            success:    fn.on_success(data, status, request),
+            error:      fn.users_error(request, status, error)
+        })
+    },
+},
+data() {
+    const store = inject("store")
+    return {
+        search_type:    'title',
+        search_term:    '',
+        store:          store
+    }
+}
+}
+let lo = function (to_log) {
+console.log(to_log)
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -1,42 +1,19 @@
 import { reactive, watch } from "vue";
 
-const STATE_NAME = "pomodoroState";
+const STATE_NAME = "film_picker_datastore";
 
 
 const defaultState = {
-
-  todos:[],
-  completedTodos:[],
-  currentTodo: null,
-  text: '',
-  step: 0,
-  todoLimit:10,
-  point:0,
-  totalStep: 0 ,
-  session: 1,
-  timeChoose: 25 ,
-  isFinished:false,
-  reward:null,
-  primary:'red-3',
-  secondary:'red-8',
-  addNewTodo:false,
-
-  focus:25,
-  short:5,
-  long:15,
-  isLong:false,
-
-  reward1:"Social media",
-  reward2:"Drink(Tea coffe etc)",
-  reward3:"short game",
-  reward4:"Short YouTube video",
-  reward5:"Meditate",
-  reward6:"Favorite music",
-  
+    sections: {
+        add_film:   {
+          search_results: {}
+        }
+    }
 }
+
 const getDefaultState = () => {
   if (localStorage.getItem(STATE_NAME) !== null) {
-    return JSON.parse(localStorage.getItem(STATE_NAME));
+  return JSON.parse(localStorage.getItem(STATE_NAME));
   }
 
   return defaultState;
@@ -45,28 +22,10 @@ const getDefaultState = () => {
 const state = reactive(getDefaultState());
 
 const methods = {
-  addTask(todo){
-    if (state.todos.length < state.todoLimit){
-    todo ? state.todos.push(todo) : ''
-    state.text = ''
-  }
-  },
-  deleteTask(todo){
-    state.todos = state.todos.filter(item => item != todo)
-    state.totalStep = state.totalStep + state.step
-    if (state.currentTodo == todo){
-      state.currentTodo = null
-    }
-  },
-  addCompletedTask(todo){
-    todo ? state.completedTodos.push(todo) : ''
-    methods.deleteTask(todo)
-  },
-  setColor(color){
-    state.primary = `${color}-3`
-    state.secondary = `${color}-8`
-  },
-  
+  // addTask(todo){
+  //   state.text = ''
+  // }
+
 }
 
 const getters = {}
@@ -74,7 +33,7 @@ const getters = {}
 watch(
   () => state,
   () => {
-    localStorage.setItem(STATE_NAME, JSON.stringify(state));
+  localStorage.setItem(STATE_NAME, JSON.stringify(state));
   },
   { deep: true }
 );
