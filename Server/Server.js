396e56f729dde0e3ace030c084f0d6e68bdf108d
tog22 <tom.ash.github@philosofiles.com>
2022-10-28 11:53:32 -0700

    Server.js: basic structure, eg for users

--- Server/Server.js
+++ Server/Server.js
@@ -1,61 +1,55 @@
 import tog from '../Libraries/tog'
 import $ from 'jquery'
 
-let server = {
-    api: {
-        load:
-        function(
-            vue_data_prop,
-            handler,
-            vue_component,
-            data,
-            status,
-            request
-        ) {
-            success(data, status, request) {
-                let response = JSON.parse(data)
-                if (response.result === 'success') {
-                    vue_component[vue_data_prop] = handler();// handler = tog.arrays.map_to_object(response.users, 'uid')
-                    tog.vue.mark_loaded(vue_data_prop, vue_component)
-                }
-            },
-            error(request, status, error) {
-                alert('⚠️ Error loading '+vue_data_prop)
-            }
-        },
-        get_group_users:
-        function(gid, vue_component) {
-            
-            let load_users_url = 'https://filmpicker.philosofiles.com/sync/?action=get_group_users&group='+gid
+let on = {
+	success:
+	function(
+		vue_data_prop,
+		handler,
+		vue_component,
+		data,
+		status,
+		request
+	) {
+		let response = JSON.parse(data)
+		if (response.result === 'success') {
+			vue_component[vue_data_prop] = handler();// handler = tog.arrays.map_to_object(response.users, 'uid')
+			tog.vue.mark_loaded(vue_data_prop, vue_component)
+		}
+	},
 
-            this.api.load(
-                'users', 
-                () => {tog.arrays.map_to_object(response.users, 'uid')},
-                vue_component
-            )
-
-            $.ajax({
-                url:        load_users_url,
-                success:    function(data, status, request) {
-                    load.users(data, status, request)
-                },
-                error:      function(request, status, error){
-                    load.users_error(request, status, error)
-                }
-            })
+	error:
+	function(error_for, request, status, error) {
+		alert('⚠️ API error for '+error_for)
+	}
     
-            $.ajax({
-                url:        load_films_url,
-                success:    function(data, status, request) {
-                    load.films(data, status, request)
-                },
-                error:      function(request, status, error) {
-                    load.films_error(request, status, error)
-                }
-            })
-        }
-    },
+}
 
+let api = {
+
+	get_group_users:
+	function(gid, vue_component) {
+		
+		let load_users_url = 'https://filmpicker.philosofiles.com/sync/?action=get_group_users&group='+gid
+
+		$.ajax({
+			url:        load_users_url,
+			success:    function(data, status, request) {
+				on_response.success(
+					'users', 
+					() => {tog.arrays.map_to_object(response.users, 'uid')},
+					vue_component,
+					data,
+					status,
+					request
+				)
+			},
+			error:      function(request, status, error){
+				on_response.error('users', request, status, error)
+			}
+		})
+
+	}
 
 }
 
@@ -66,4 +60,4 @@ let server = {
 // func
 
 
-export default server
+export default api
